d65e3289b9d04010917b984ac019fdec31e458a2
David Pärsson <david@parsson.se>
2013-01-30 12:00:18 +0100

    Made it possible to configure search and print patterns from command line

--- search-logs.py
+++ search-logs.py
@@ -6,11 +6,10 @@ import optparse
 import datetime
 import re
 
-SEARCH_PATTERN = '\[([^\]]+)\] Retrying testcase ([^ ]+)'
-PRINT_PATTERN = '%s.%s'
+DEFAULT_SEARCH_PATTERN = '\[([^\]]+)\] Retrying testcase ([^ ]+)'
+DEFAULT_PRINT_PATTERN = '%s.%s'
 
 verbose = False
-search_pattern = re.compile(SEARCH_PATTERN)
 
 def parse(url, tree=None):
   if url[-1] != "/":
@@ -20,16 +19,17 @@ def parse(url, tree=None):
     url += "?tree=%s" % tree
   return ast.literal_eval(urllib.urlopen(url).read())
 
-def get_matched_lines(url):
+def get_matched_lines_in(url, search_pattern, print_pattern):
   lines = urllib.urlopen(url).readlines()
-  groups_from_lines = map(lambda line: search_pattern.findall(line), lines)
+  search_pattern_regex = re.compile(search_pattern)
+  groups_from_lines = map(lambda line: search_pattern_regex.findall(line), lines)
   matched_lines = filter(lambda line: bool(line), groups_from_lines)
   return map(lambda line: line[0], matched_lines)
 
 def get_date(timestamp):
    return datetime.datetime.fromtimestamp(int(timestamp)/1000)
 
-def find_committers(url):
+def get_matched_log_lines(url, search_pattern, print_pattern):
   job = parse(url, "builds[result,number,timestamp],displayName")
   builds = job['builds']
 
@@ -38,9 +38,9 @@ def find_committers(url):
     console_url = "%s%d/consoleText" % (url, build_number)
     print '#%d - %s - %s - %s' % (build_number, job['displayName'], get_date(build['timestamp']), build['result'])
     print_if_verbose(console_url)
-    matched_lines = get_matched_lines(console_url)
+    matched_lines = get_matched_lines_in(console_url, search_pattern, print_pattern)
     for matched_line in matched_lines:
-      print '%s.%s' % matched_line
+      print print_pattern % matched_line
     print
 
 
@@ -58,10 +58,14 @@ Gets yellow committers for all jobs in the supplied Jenkins view.""")
     help="Prints progress, instead of only the revision")
   parser.add_option("-d", "--debug", action="store_true", default=False,
     help="Prints stack traces")
+  parser.add_option("-s", "--search", default=DEFAULT_SEARCH_PATTERN,
+    help="Search pattern, regular expression")
+  parser.add_option("-o", "--output", default=DEFAULT_PRINT_PATTERN,
+    help="Print pattern, printing groups")
   try:
     (options, (url,)) = parser.parse_args()
     verbose = options.verbose
-    find_committers(url)
+    get_matched_log_lines(url, options.search, options.output)
   except ValueError, e:
     if options.debug:
       raise e, None, sys.exc_info()[2]
